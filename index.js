5dee122951dc14c8fbb6bea2f37ce5c3189fcb0a
OlderFox <postmaster@aliserver.net>
2021-11-30 23:43:49 +0800

    Fix

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const file = require('./modules/file')
 const {connectToServer} = require('./modules/socket')
 
 async function main(){
-    await file.initConfig()
-    await file.readPsk()
-    await file.updatePsk('li990114')
     connectToServer()
 }
 
